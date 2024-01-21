0ec90d29d0383fcf5315ade77ecf0fa52f58ab87
azzadi <azzadi241@gmail.com>
2024-01-21 21:03:12 +0700

    Create Menu.php

---
+++ helpers/Menu.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'user', 
+			'label' => 'User', 
+			'icon' => '<i class="fa fa-user-secret "></i>'
+		),
+		
+		array(
+			'path' => 'laporan_kejadian', 
+			'label' => 'Laporan Kejadian', 
+			'icon' => '<i class="fa fa-database "></i>'
+		),
+		
+		array(
+			'path' => 'role_permissions', 
+			'label' => 'Role Permissions', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'roles', 
+			'label' => 'Roles', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
